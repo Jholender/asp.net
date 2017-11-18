a08d2790f81a54abdfa27aeb04988595239c64ba
Jarek Holender <jholender@op.pl>
2017-11-18 11:18:45 +0100

    kommit

---
+++ WebApplication1/WebApplication1/BussinesLogic/FuelBusinesLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.BussinesLogic
+{
+    public class FuelBusinesLogic
+    {
+        public double CalculateAvgConsumption(int fuel, int distance)
+        {
+            double avg = ((double)fuel / distance) * 100;
+            return avg;
+
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/FeulController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.BussinesLogic;
+using WebApplication1.Models;
+using WebApplication1.ViewModel;
+
+namespace WebApplication1.Controllers
+{
+    public class FeulController : Controller
+    {
+        private readonly FuelBusinesLogic _busienssLogic;
+  
+
+        public FeulController()
+        {
+            _busienssLogic = new FuelBusinesLogic();
+        }
+        
+       
+        // GET: Feul
+        public ActionResult Index()
+        {
+            var model = new FuelViewModel { FuelModel = new FuelModel()};
+            return View("View",model);
+        }
+        [HttpPost]
+        public ActionResult FeulCalculate(FuelViewModel model)
+        {
+            if (model.FuelModel != null && model.FuelModel.Distance != 0)
+            {
+                model.FuelModel.AvgConsumption = _busienssLogic.CalculateAvgConsumption(model.FuelModel.Fuel, model.FuelModel.Distance);
+                model.ShowResult = true;
+                return View("View", model);
+            }
+            model.ShowResult = false;
+            ViewBag.ERROR = "Bledne dane";
+            return View("View", model);
+        }
+    }
+}
